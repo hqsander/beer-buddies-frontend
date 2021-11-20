51085c50a53fac83e152ae6236a2253eb45cc0da
Henrique Sander Fernandes <hqsander@gmail.com>
2021-11-20 09:15:09 -0300

    Implementa service para login.

---
+++ src/app/service/authentication.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpResponse,
+} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  private host = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>(
+      `${this.host}/user/login`,
+      user,
+      { observe: 'response' }
+    );
+  }
+}
