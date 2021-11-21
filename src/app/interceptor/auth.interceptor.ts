d6d2a377b45da4a98452b852e24292689a96a47a
Henrique Sander Fernandes <hqsander@gmail.com>
2021-11-21 10:58:15 -0300

    Implementa interceptor para adicionar token no header.

---
+++ src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../service/authentication.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authenticationService: AuthenticationService) {}
+
+  intercept(httpRequest: HttpRequest<any>, httpHandler: HttpHandler): Observable<HttpEvent<any>> {
+    if (httpRequest.url.includes(`${this.authenticationService.host}/user/login`)) {
+      return httpHandler.handle(httpRequest);
+    }
+    if (httpRequest.url.includes(`${this.authenticationService.host}/user/register`)) {
+      return httpHandler.handle(httpRequest);
+    }
+    this.authenticationService.carregarToken();
+    const token = this.authenticationService.getToken();
+    const requestTratada = httpRequest.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
+    return httpHandler.handle(requestTratada);
+  }
+}
--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -9,7 +9,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
   providedIn: 'root',
 })
 export class AuthenticationService {
-  private host = environment.apiUrl;
+  public host = environment.apiUrl;
   private token: string | null | undefined;
   private usernameLogado: string | null | undefined;
   private JwtHelper = new JwtHelperService();
