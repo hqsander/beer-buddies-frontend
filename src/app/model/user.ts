25b20f99a47f3918dbbf6c1ebf8ce967e5291889
Henrique Sander Fernandes <hqsander@gmail.com>
2021-11-21 09:52:39 -0300

    Implementa chamadas do serviço usuário.

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,1 +1,27 @@
-export class User {}
+export class User {
+  public id: number | undefined;
+  public userId: string | undefined;
+  public nome: string;
+  public sobrenome: string;
+  public email: string;
+  public urlAvatar: string | undefined;
+  public dataAcesso: Date | undefined;
+  public dataCriacao: Date | undefined;
+  public password: string | undefined;
+  public username: string;
+  public role: string;
+  public authorities: [];
+  public isEnabled: boolean;
+  public isAccountNonLocked: boolean;
+
+  constructor() {
+    this.nome = '';
+    this.sobrenome = '';
+    this.email = '';
+    this.username = '';
+    this.role = '';
+    this.authorities = [];
+    this.isEnabled = false;
+    this.isAccountNonLocked = false;
+  }
+}
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,55 @@
+import { HttpClient, HttpErrorResponse, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../model/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private host = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public criar(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/criar`, formData);
+  }
+
+  public listar(): Observable<User[] | HttpErrorResponse> {
+    return this.http.get<User[]>(`${this.host}/user/listar`);
+  }
+
+  public atualizar(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/atualizar`, formData);
+  }
+
+  public excluir(username: string): Observable<any | HttpErrorResponse> {
+    return this.http.delete<any>(`${this.host}/user/excluir/${username}`);
+  }
+
+  public redefinirSenha(email: string): Observable<any | HttpErrorResponse> {
+    return this.http.get(`${this.host}/user/redefinirSenha/${email}`);
+  }
+
+  public atualizarAvatar(formData: FormData): Observable<HttpEvent<User> | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/atualizarAvatar`, formData, {
+      reportProgress: true,
+      observe: 'events',
+    });
+  }
+
+  public criarUserFormData(usernameLogado: string, user: User, avatar: File): FormData {
+    const formData = new FormData();
+    formData.append('currentUsername', usernameLogado);
+    formData.append('nome', user.nome);
+    formData.append('sobrenome', user.sobrenome);
+    formData.append('email', user.email);
+    formData.append('avatar', avatar);
+    formData.append('role', user.role);
+    formData.append('isEnabled', JSON.stringify(user.isEnabled));
+    formData.append('isAccountNonLocked', JSON.stringify(user.isAccountNonLocked));
+    formData.append('username', user.username);
+    return formData;
+  }
 }
