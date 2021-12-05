13858e65bad15e0f8dcbacdfd533d9e5ae46edf7
Henrique Sander Fernandes <hqsander@gmail.com>
2021-12-05 09:52:20 -0300

    Habilita AuthenticationGuard na rota principal.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { PrincipalComponent } from './component/principal/principal.component';
+import { AuthenticationGuard } from './guard/authentication.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'principal', component: PrincipalComponent },
+  { path: 'principal', component: PrincipalComponent, canActivate: [AuthenticationGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
